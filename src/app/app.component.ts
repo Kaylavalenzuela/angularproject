981c3bff77238c2caa9f6cfcbbc36a4b6c5c9227
Kaylavalenzuela <kaylalaneevalenzuela@gmail.com>
2017-10-19 22:22:41 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = "kayla's app";
+  pets: Array<Pet> = [];
+
+  constructor(){}
+
+  ngOnInit() {
+    this.pets = [
+      {
+        name: 'Rocksie',
+        type: 'Dog',
+        age: 7
+      },
+      {
+        name: 'Minerva',
+        type: 'Cat',
+        age: 5
+      },
+      {
+        name: 'Mu Shu',
+        type: 'Dog',
+        age: 2
+      }
+    ]
+  }
+}
+
+export class Pet {
+  name: string;
+  type: string;
+  age: number;
 }
